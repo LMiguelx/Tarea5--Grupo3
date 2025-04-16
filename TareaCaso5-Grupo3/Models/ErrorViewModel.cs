8a996f5230eb2029dc02608eef7b86733b4dd36e
Miguel <144188401+LMiguelx@users.noreply.github.com>
2025-04-16 08:54:36 -0500

    creacion proyecto

---
+++ TareaCaso5-Grupo3/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TareaCaso5_Grupo3.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
